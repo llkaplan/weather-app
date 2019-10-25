861ea39278f7da4d16549fccbe0b4bc01bf73276
Leyla Kaplan <leylakaplan@leylas-mbp.lan>
2019-10-25 15:45:13 -0700

    refining the formatting of the html doc and adding API key

---
+++ script.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    var appID = "9ebf11cb2c5cda468f63c5e3c43699b4";
+
+    $(".query_btn").click(function(){
+        var query_param = $(this).prev().val();
+    })
+});
